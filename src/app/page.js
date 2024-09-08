001bb818d678691543f32eeedc5248bff25c4fc5
NateLog <nachocheese1807@gmail.com>
2024-09-08 13:33:06 +0100

    we're in sync

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div className={styles.main}>body</div>
+      <div className={styles.main}>body is this working</div>
       <Footer />
     </div>
   );
